35c4b690573ef80f8c89b70ff14c4adb9d17a1fc
Carlos Fernandez <jcfernan@live.com>
2020-07-31 15:05:39 -0600

    Clean up

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom'
     return(
             <nav className="nav-wrapper">
                 <div className="container">
-                    <Link to="/" className="brand-logo">Shopping</Link>
+                    <Link to="/" className="brand-logo">Make a Joyful Sound</Link>
                     
                     <ul className="right">
-                        <li><Link to="/">Shop</Link></li>
-                        <li><Link to="/cart">My cart</Link></li>
+                        <li><Link to="/">Find Joy</Link></li>
+                        {/* <li><Link to="/cart">My cart</Link></li> */}
                         <li><Link to="/cart"><i className="material-icons">shopping_cart</i></Link></li>
                     </ul>
                 </div>
